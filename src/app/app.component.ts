0c55a1a41cbbe80da100ff349e3a46238c0d8191
Baron78x <nico78nb@gmail.com>
2022-09-17 16:45:45 +0200

    pipe + road ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit {
-  pokemonsList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-  ngOnInit(): void {
-    console.table(this.pokemonsList);
-  }
-
-  selectPokemon(pokemonId: String) {
-    const pokemon: Pokemon|undefined = this.pokemonsList.find(pokemon => pokemon.id == +pokemonId);
-    if(pokemon){
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    }else{
-      console.log(`Vous avez demandé un pokémon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-    }
-    
-  }
+export class AppComponent {
+  
 }
